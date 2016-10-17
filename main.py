e908fd8f41c33cc21df9cd0667d276870a104f8b
Diego Dukão <diegodukao@gmail.com>
2016-10-17 11:49:24 -0200

    method generate first version

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from kivy.app import App
+from random import shuffle
+
+WORDS = [
+    "vagabundo",
+    "skate",
+    "charlie",
+    "brown",
+    "santos",
+]
 
 
 class ChoraoIpsum(App):
-    pass
 
+    def generate(self, qt_words):
+        words = WORDS.copy()
+        sentence = ""
+        for i in range(int(qt_words)):
+            if not words:
+                words = WORDS.copy()
+            shuffle(words)
+            sentence = "{} {}".format(sentence, words.pop())
+
+        print(sentence)
 
 if __name__ == "__main__":
     ChoraoIpsum().run()
